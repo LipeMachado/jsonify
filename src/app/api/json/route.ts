2cab550440feaaf53ef28da194245d2391e5f7cb
LipeMachadoVezos <felipe.machado@vezos.com.br>
2024-07-11 15:36:02 -0300

    Init Commit

---
+++ src/app/api/json/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from "next/server"
+import { format } from "path";
+import { z } from 'zod';
+
+export const POST = async (req: NextRequest) => {
+
+    const body = await req.json()
+
+    const genericSchema = z.object({
+        data: z.string(),
+        format: z.object({}).passthrough(),
+    })
+
+    const res = genericSchema.parse(body)
+    console.log("res", res)
+
+    return new Response('OK')
+}
