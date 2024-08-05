18c1ddd71d82e865471bd7002b4876f5c8a3412a
LipeMachado <silvalipe765@gmail.com>
2024-08-05 15:17:52 -0300

    Create a schema from the expected user format

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,9 +1,46 @@
 import { NextRequest } from "next/server"
 import { format } from "path";
-import { z } from 'zod';
+import { z, ZodTypeAny } from 'zod';
+
+const determineSchemaType = (schema: any) => {
+    if(!schema.hasOwnProperty("type")) {
+        if(Array.isArray(schema)) {
+            return "array"
+        } else {
+            return typeof schema
+        }
+    }
+}
+
+const jsonSchemaToZod = (schema: any): ZodTypeAny => {
+    const type = determineSchemaType(schema);
+
+    switch(type) {
+        case "string":
+            return z.string().nullable()
+        case "number":
+            return z.number().nullable()
+        case "boolean":
+            return z.boolean().nullable()
+        case "array":
+            return z.array(jsonSchemaToZod(schema.items)).nullable()
+        case "object":
+            const shape: Record<string, ZodTypeAny> = {}
+
+            for(const key in schema) {
+                if(key !== "type"){
+                    shape[key] = jsonSchemaToZod(schema[key])
+                }
+            }
+
+            return z.object(shape)
+
+        default:
+            throw new Error(`Unsupported schema type: ${type}`)
+    }
+}
 
 export const POST = async (req: NextRequest) => {
-
     const body = await req.json()
 
     const genericSchema = z.object({
@@ -11,8 +48,18 @@ export const POST = async (req: NextRequest) => {
         format: z.object({}).passthrough(),
     })
 
-    const res = genericSchema.parse(body)
-    console.log("res", res)
+    const {data, format } = genericSchema.parse(body)
+
+    //create a schema from the expected user format    
+    const dynamicSchema = jsonSchemaToZod(format)
+
+    class RetryablePromise<T> extends Promise<T> {
+        static async retry(){
+
+        }
+    }
+
+    RetryablePromise.retry
 
     return new Response('OK')
 }
